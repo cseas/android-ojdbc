18f0205af2084e2d30e02b711fca22afc8f112f3
cseas <contact.abhijeetsingh@gmail.com>
2019-01-28 00:19:04 +0530

    Add MainActivity

--- Query/app/src/main/java/com/absingh/query/MainActivity.java
+++ Query/app/src/main/java/com/absingh/query/MainActivity.java
@@ -1,13 +1,80 @@
 package com.absingh.query;
 
+/**
+ * MainActivity.java
+ * Created by Abhijeet Singh
+ * www.absingh.com
+ */
+
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.absingh.query.helper.Util;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String DEFAULT_DRIVER = "oracle.jdbc.driver.OracleDriver";
+    private static final String DEFAULT_URL = "jdbc:oracle:thin:@192.168.43.47:1521:XE";
+    private static String DEFAULT_USERNAME;
+    private static String DEFAULT_PASSWORD;
+
+    private Connection connection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        TextView tv = (TextView) findViewById(R.id.hello);
+        try {
+            this.connection = createConnection();
+            Toast.makeText(MainActivity.this, "Connected",
+                    Toast.LENGTH_SHORT).show();
+            Statement stmt=connection.createStatement();
+            StringBuffer stringBuffer = new StringBuffer();
+            ResultSet rs=stmt.executeQuery("select * from student");
+            while(rs.next()) {
+                stringBuffer.append( rs.getString(1)+"\n");
+            }
+            tv.setText(stringBuffer.toString());
+            connection.close();
+        }
+        catch (Exception e) {
+
+            Toast.makeText(MainActivity.this, ""+e,
+                    Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection createConnection(String driver, String url, String username, String password) throws ClassNotFoundException, SQLException {
+
+        Class.forName(driver);
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    public Connection createConnection() throws ClassNotFoundException, SQLException {
+        try {
+            DEFAULT_USERNAME = Util.getProperty("username",getApplicationContext());
+            DEFAULT_PASSWORD = Util.getProperty("password",getApplicationContext());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return createConnection(DEFAULT_DRIVER, DEFAULT_URL, DEFAULT_USERNAME, DEFAULT_PASSWORD);
     }
 }
