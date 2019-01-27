d7ce5f9b05c30fd06159bf7af57854c4412a37fb
cseas <contact.abhijeetsingh@gmail.com>
2019-01-28 00:43:00 +0530

    Display complete query result

--- Query/app/src/main/java/com/absingh/query/MainActivity.java
+++ Query/app/src/main/java/com/absingh/query/MainActivity.java
@@ -18,6 +18,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -47,10 +48,26 @@ public class MainActivity extends AppCompatActivity {
                     Toast.LENGTH_SHORT).show();
             Statement stmt=connection.createStatement();
             StringBuffer stringBuffer = new StringBuffer();
-            ResultSet rs=stmt.executeQuery("select * from student");
-            while(rs.next()) {
-                stringBuffer.append( rs.getString(1)+"\n");
+            ResultSet rs = stmt.executeQuery("select * from student");
+
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            // print column names
+            for(int i=1; i<=columnsNumber; i++) {
+                stringBuffer.append(rsmd.getColumnName(i) + "\t\t\t\t");
             }
+            stringBuffer.append("\n");
+
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1)
+                        stringBuffer.append(",\t\t\t");
+                    String columnValue = rs.getString(i);
+                    stringBuffer.append(columnValue);
+                }
+                stringBuffer.append("\n");
+            }
+
             tv.setText(stringBuffer.toString());
             connection.close();
         }
